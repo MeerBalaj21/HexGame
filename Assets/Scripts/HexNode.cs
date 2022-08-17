5f23644cde4e07464df4f577fcfe469dbcd39cc5
Meer.Balaj <meer.balaj@mindstudios.com>
2022-08-17 13:28:01 +0500

    node stores  indexes on generation

---
+++ Assets/Scripts/HexNode.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexNode : MonoBehaviour
+{
+    public int x;
+    public int y;
+
+    public void SetX(int _x)
+    {
+        x = _x;
+    }
+    public void SetY(int _y)
+    {
+        y = _y;
+    }
+
+}
--- Assets/Scripts/LevelGeneration.cs
+++ Assets/Scripts/LevelGeneration.cs
@@ -11,16 +11,23 @@ public class LevelGeneration : MonoBehaviour
     [SerializeField] private float _xOffset;
     [SerializeField] private float _yOffset;
 
+    public HexNode _hex;
+
 
     private void Start()
     {
-        for(int x = 0; x < _rows; x++)
+        GridSetup();
+    }
+
+    private void GridSetup()
+    {
+        for (int x = 0; x < _rows; x++)
         {
-            for(int y = 0; y < _columns; y++)
+            for (int y = 0; y < _columns; y++)
             {
                 float xPos = x * _xOffset;
 
-                if( y % 2 == 1)
+                if (y % 2 == 1)
                 {
                     xPos = xPos + (_xOffset / 2);
 
@@ -33,9 +40,13 @@ public class LevelGeneration : MonoBehaviour
                 GameObject HexGo = (GameObject)Instantiate(Hex, new Vector2(xPos, y * _yOffset), Quaternion.identity);
 
                 HexGo.name = "Hex_" + x + ", " + y;
+                _hex = HexGo.GetComponent<HexNode>();
+                _hex.SetX(x);
+                _hex.SetY(y);
                 HexGo.transform.SetParent(this.transform);
 
             }
         }
+    
     }
 }
